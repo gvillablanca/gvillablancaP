d2ea3ac4772e7c3f7f23d3e273e6d8f624ae5bb7
gvillablanca <g.villalobo.m@gmail.com>
2018-03-09 08:28:29 -0300

    efecto transicion, disminucion velocidad boton up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){ 
-$('elemento-a-animar').smoove(options);
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-$('section').smoove({offset:'40%'});
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 
 //boton de volver a inicio
   $('.ir-arriba').click(function(){
     $('body, html').animate({
       scrollTop: '0px'
-    }, 300);
+    }, 700);
   });
  
   $(window).scroll(function(){
@@ -38,6 +56,3 @@ $(document).ready(function(){
 
 });
 
-function procesar(xform){
-  window.open(xform, 'nventana', 'width=450,height=300,status=yes,resizable=yes,scrollbars=yes');
-}
